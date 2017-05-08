d69e109d0d5b1249c3e0b312d3f5c7f5e59f02c9
Felipe <felipe.silvalmeida@gmail.com>
2017-05-08 08:52:06 -0300

    Commit dos arquivos do primeiro-projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+import {CursosModule} from './cursos/cursos.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { InputPropertyComponent } from './input-property/input-property.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component,
+    DataBindingComponent,
+    InputPropertyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
